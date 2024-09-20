4fc73759faa2c0e0cbdd48817f53ea7643878437
DevAndresMunoz <andresgabrielmunoz@gmail.com>
2024-09-19 19:05:08 -0600

    added module.exports to js file

---
+++ stringMethods.js
@@ -0,0 +1,12 @@
+module.exports = {
+    getCharAt,
+    concatenateStrings,
+    doesInclude,
+    getIndexOf,
+    sliceString,
+    splitString,
+    convertToLowerCase,
+    convertToUpperCase,
+    trimString,
+    replaceSubstring
+};
