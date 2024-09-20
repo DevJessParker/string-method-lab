f33b2b078042bfc321a7f72c2f9271127ef16294
DevAndresMunoz <andresgabrielmunoz@gmail.com>
2024-09-19 19:57:08 -0600

    passing first two tests for getcharat

---
+++ src/stringMethods.js
@@ -0,0 +1,50 @@
+function getCharAt(str) {
+    if (str) {
+        let testString = str;
+        return testString.charAt(1);
+    } else {
+        return 'String is empty'
+    }
+}
+
+function concatenateStrings() {}
+
+
+function doesInclude() {}
+
+
+function getIndexOf() {}
+
+
+function sliceString() {}
+
+
+function splitString() {}
+
+
+function convertToLowerCase() {}
+
+
+function convertToUpperCase() {}
+
+
+function trimString() {}
+
+
+function replaceSubstring() {}
+
+
+
+
+module.exports = {
+    getCharAt,
+    concatenateStrings,
+    doesInclude,
+    getIndexOf,
+    sliceString,
+    splitString,
+    convertToLowerCase,
+    convertToUpperCase,
+    trimString,
+    replaceSubstring
+};
